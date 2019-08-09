c4006ca6bdc69d5d375eb1ad5b5ab159781a52f1
Mark Post <mark.post@live.nl>
2019-08-09 16:36:59 +0200

    Add JavaDoc to accounts

--- AccountsMicroservice/src/main/java/xyz/markpost/accounts/service/AccountService.java
+++ AccountsMicroservice/src/main/java/xyz/markpost/accounts/service/AccountService.java
@@ -6,65 +6,65 @@ import xyz.markpost.accounts.dto.AccountResponseDTO;
 import xyz.markpost.util.dto.TransactionType;
 
 /**
- *
+ * Business logic for accounts
  */
 public interface AccountService {
 
   /**
-   *
-   * @param accountRequestDTO
-   * @return
+   * Create a new account
+   * @param accountRequestDTO The data for creating an account
+   * @return The response dto for the created account
    */
   AccountResponseDTO create(AccountRequestDTO accountRequestDTO);
 
   /**
-   *
-   * @param id
-   * @return
+   * Find an account based on its' ID
+   * @param id The ID of the account to search
+   * @return The response DTO of the found account or null
    */
   AccountResponseDTO findById(Long id);
 
   /**
-   *
-   * @param clientId
-   * @return
+   * Find all accounts with this client ID
+   * @param clientId The client ID to use
+   * @return The found accounts or an empty list
    */
   List<AccountResponseDTO> findByClientId(Long clientId);
 
   /**
-   *
-   * @return
+   * Find all accounts in the system
+   * @return All accounts or empty list
    */
   List<AccountResponseDTO> findAll();
 
   /**
-   *
-   * @param id
-   * @param accountRequestDTO
-   * @return
+   * Update account with given data
+   * @param id The ID of the account to update
+   * @param accountRequestDTO The data to update
+   * @return The response dto of the updated account
    */
   AccountResponseDTO update(Long id, AccountRequestDTO accountRequestDTO);
 
   /**
-   *
-   * @param id
-   * @param amount
-   * @return
+   * Check if the account has sufficient balance
+   * @param id The ID of the account
+   * @param amount The amount the account should minimally have
+   * @return Whether or not the account has sufficient balance
    */
   boolean checkBalance(Long id, float amount);
 
   /**
-   *
-   * @param id
-   * @param amount
-   * @param type
-   * @return
+   * Update the balance of the account
+   * @param id The ID of the account where the amount needs to be updated
+   * @param amount The amount to add or remove to the current amount
+   * @param type The TransactionType to apply
+   * @return Whether or not the action succeeded
    */
   boolean updateBalance(Long id, float amount, TransactionType type);
 
   /**
-   *
-   * @param id
+   * Delete the account
+   * @param id The ID of the account to delete
    */
   void delete(Long id);
 }
